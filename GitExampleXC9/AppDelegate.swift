358eca72dd8e1a1b769d0e2643fd817e66d3ab34
AmberTripp1983 <amberlynntripp@gmail.com>
2018-04-27 16:41:43 -0500

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by Amber Tripp on 4/27/18.
+//  Copyright © 2018 Amber Tripp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Amber Tripp on 4/27/18.
+//  Copyright © 2018 Amber Tripp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
